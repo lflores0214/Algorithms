f3ec355b7ce94ee21bccc6beb154270ef88ba2ab
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-18 14:52:34 -0700

    finished with recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python
 
 import math
-
+'''
+receives two dictionaries
+compare the values of the recipe and ingredients
+if you don't have an ingredient called for in the recipe return 0
+if the ingredient value is less than the corresponding recipe value return 0
+divide // the value of ingredients by the value of recipes to get the number of batches you'll be able to make
+'''
 def recipe_batches(recipe, ingredients):
-  pass 
+  total_batches = 100
+  for i in recipe:
+    # if the recipe calls for something not in the ingredients return 0
+    if ingredients.get(i) == None:
+      return 0
+    # if the recipe calls for more than whats available
+    if recipe[i] > ingredients[i]:
+      return 0
+    # this has to be down here because the fn will break if there is no ingredient[i] so it has to go through the check first
+    current_batches = ingredients[i] // recipe[i]
+    # if the current number of batches is less than the total_batches
+    if current_batches < total_batches:
+      # reassign total_batches to current_batches
+      total_batches = current_batches
+  return total_batches
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+print(recipe_batches(
+  { 'milk': 100, 'butter': 50, 'flour': 5 },
+  { 'milk': 138, 'butter': 48, 'flour': 51 }))
+
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 
 import argparse
-
+'''
+receives a list of of prices
+supposed to return the max profit
+can only sell if the arr if you already bought
+'''
 def find_max_profit(prices):
+  min_price = 0
+  max_profit = 0
   for i in range(len(prices)):
-    
+    for j in range(len(prices)):
+      if prices[i] < prices[j]:
+        min_price = prices[i]
+  print(min_price)
+
+my_arr = [1050, 270, 1540, 3800, 2]
+find_max_profit([1050, 270, 1540, 3800, 2])
 
 
 if __name__ == '__main__':
